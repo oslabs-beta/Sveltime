c97761671178138d5793cc0d4ad6d0af40ef9ac0
usr193 <guy.unix@gmail.com>
2022-05-18 08:23:12 -0700

    able to inject script

--- extension/js/content.js
+++ extension/js/content.js
@@ -1,2 +1,16 @@
-console.log("This is the content script");
-// chrome.runtime.sendMessage('contentScript');
+
+const code = `(function() {
+console.log('This is the content script');
+window.document.addEventListener('SvelteRegisterComponent', (e) => {
+  console.log('svelteregisterComponent: e.detail ', e.detail); });
+})()`;
+
+document.documentElement.setAttribute('onreset', code);
+document.documentElement.dispatchEvent(new CustomEvent('reset'));
+
+
+// window.document.addEventListener('SvelteDOMInsert', e => {
+//   console.log('svelteDOMinsert e: ', e);
+// });
+// window.document.addEventListener('SvelteDOMRemove', e => { console.log('svelteDOMRemove e: ', e); });
+// window.document.addEventListener('SvelteDOMAddEventListener', e => {console.log('svelteDOMAddEventListener e :', e);});
